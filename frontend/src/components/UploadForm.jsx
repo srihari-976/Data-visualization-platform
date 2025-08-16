97c0b5367c3a2ecabf0add28d1782ad50084ea5f
SRIHARI R <120409108+srihari-976@users.noreply.github.com>
2025-08-16 15:30:59 +0530

    Update UploadForm.jsx

--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -27,7 +27,7 @@ import { firebaseService } from '../services/firebaseService';
 
 // Configure axios defaults
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = '100.20.92.101:5000';
+axios.defaults.baseURL = 'https://data-visualization-platform.onrender.com';
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
